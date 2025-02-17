0df8bf9112b7d5c934ae714efdba01dc7d8ff510
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-17 20:33:12 +0100

    rename components

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,24 +1,24 @@
 import react from "react";
 
-const Square = () => {
+const Board = () => {
   return (
-    <main className="square">
-      <div className="square-row">
-        <button>X-1</button>
-        <button>X-2</button>
-        <button>X-3</button>
+    <main>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
       </div>
-      <div className="square-row">
-        <button>X-4</button>
-        <button>X-5</button>
-        <button>X-6</button>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
       </div>
-      <div className="square-row">
-        <button>X-7</button>
-        <button>X-8</button>
-        <button>X-9</button>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
       </div>
     </main>
   );
 };
-export default Square;
+export default Board;
