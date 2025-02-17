47b1234165821550079aa041027478dd5ff92086
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-17 18:55:49 +0100

    putting it togther

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,18 +1,24 @@
 import react from "react";
 
- const Square = () =>{
+const Square = () => {
   return (
-    <div className="square">
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-    </div>
-  )
- }
+    <main className="square">
+      <div className="square-row">
+        <button>X-1</button>
+        <button>X-2</button>
+        <button>X-3</button>
+      </div>
+      <div className="square-row">
+        <button>X-4</button>
+        <button>X-5</button>
+        <button>X-6</button>
+      </div>
+      <div className="square-row">
+        <button>X-7</button>
+        <button>X-8</button>
+        <button>X-9</button>
+      </div>
+    </main>
+  );
+};
 export default Square;
