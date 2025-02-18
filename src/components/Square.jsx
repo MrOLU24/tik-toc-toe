d2fffee8c14d23b190534c9ed1ae7e0a36752d81
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-18 14:10:28 +0100

    changing the state handling to the parent components

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
 
-const Square = ({ value }) => {
-  return <button className="square">{value}</button>;
+const Square = ({ value, onSquareClick }) => {
+  return <button onClick={onSquareClick} className="square">{value}</button>;
 };
 
 export default Square;
